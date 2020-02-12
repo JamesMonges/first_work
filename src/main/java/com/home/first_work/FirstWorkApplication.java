ce7ee0258b5b072355707dbaf85290ccc8156b77
chenziww <1768585378@qq.com>
2020-02-12 19:34:05 +0800

    Initial commit

---
+++ src/main/java/com/home/first_work/FirstWorkApplication.java
@@ -0,0 +1,13 @@
+package com.home.first_work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstWorkApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/home/first_work/mycontroller.java
@@ -0,0 +1,13 @@
+package com.home.first_work;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+
+@Controller
+@Repository
+public class mycontroller{
+
+      public String say(){
+          return  "hellow";
+      }
+}
